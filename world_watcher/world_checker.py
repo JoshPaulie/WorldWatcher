7719d8b6b89f8d078c06577269470265e9a6c5a8
JoshPaulie (Bexli) <30572280+JoshPaulie@users.noreply.github.com>
2024-04-18 11:47:02 -0500

    add: mass ping servers asynchronously

--- world_watcher/world_checker.py
+++ world_watcher/world_checker.py
@@ -1,3 +1,4 @@
+import asyncio
 import random
 import statistics
 
@@ -9,9 +10,30 @@ worlds_servers = [f"oldschool{n}.runescape.com" for n in range(1, 101)]
 
 async def poll_servers(*, threshhold: float, pool_size: int) -> bool:
     """Determines if OSRS world servers are online by randomly polling a number of worlds. If the percent of worlds online (ie. respond back) exceeds the threshhold, the game is considered 'online'"""
+    # Random selection of servers to query
     server_pool = random.choices(worlds_servers, k=pool_size)
-    pool_response_times = [await ping(address) for address in server_pool]
-    pool_online_results = statistics.mean([bool(response_time) for response_time in pool_response_times])
-    if pool_online_results > threshhold:
+
+    # Queue so we can issue many pings asynchronously
+    queue = asyncio.Queue()
+    tasks = []
+    for address in server_pool:
+        task = asyncio.create_task(ping(address))
+        tasks.append(task)
+        await queue.put(task)
+
+    # Wait for all tasks to complete
+    await asyncio.gather(*tasks)
+
+    # Collect results from the queue
+    pool_response_times = []
+    while not queue.empty():
+        task = await queue.get()
+        pool_response_times.append(task.result())
+
+    # Determine % of servers that responded
+    pool_response_results = statistics.mean([bool(response_time) for response_time in pool_response_times])
+
+    # If exceeds theshhold, game is considered 'online'
+    if pool_response_results > threshhold:
         return True
     return False
