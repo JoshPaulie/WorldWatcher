ae5633a73a2eff71c3261968460c650721faf006
JoshPaulie (Bexli) <30572280+JoshPaulie@users.noreply.github.com>
2024-04-19 18:17:20 -0500

    fix: make ping actually async (oops)

--- world_watcher/ping.py
+++ world_watcher/ping.py
@@ -1,6 +1,6 @@
+import asyncio
 import os
 import re
-import subprocess
 
 
 class NonPosixEnvironment(Exception):
@@ -8,7 +8,7 @@ class NonPosixEnvironment(Exception):
 
 
 async def ping(address: str) -> float | None:
-    """(Very) crude wrapping around unix ping binary. Returns server response time in milliseconds
+    """(Very) crude wrapping around unix ping binary. Returns server response time in milliseconds, or None if the server is unavailable in someway.
 
     Assumptions:
         - All unix/posix environments use the same (or similar enough) ping binary
@@ -18,17 +18,24 @@ async def ping(address: str) -> float | None:
     if os.name != "posix":
         raise NonPosixEnvironment("Function only wraps posix ping command")
 
-    try:
-        ping_result = subprocess.check_output(["ping", "-c", "1", address], stderr=subprocess.PIPE)
-    except subprocess.CalledProcessError:
-        # Covers: Ping command exits with error.
-        # Likely because the server couldn't be resolved (unknown host)
+    process = await asyncio.create_subprocess_exec(
+        "ping",
+        "-c",
+        "1",
+        address,
+        stdout=asyncio.subprocess.PIPE,
+        stderr=asyncio.subprocess.PIPE,
+    )
+    stdout, stderr = await process.communicate()
+
+    if process.returncode != 0:
+        # It seems ping helpfully exits with an error if the response took too long,
+        # or if the server is unavailable.
         return None
 
-    ping_result = ping_result.decode()
+    ping_result = stdout.decode()
     response_ms = re.search(r"time=(\d+\.\d+)", ping_result)
     if response_ms:
         return float(response_ms.group(1))
-    # Covers: Server took too long to reply
-    # Covers: Server is unavailable
+    # Covers: Ping doesn't get response, but doesn't exit with error for some reason
     return None
